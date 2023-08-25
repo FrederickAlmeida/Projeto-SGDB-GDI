dbdcda2d7aa964f8f91c8255be77eb05927d2feb
Felipe Neiva <felipeneivals@gmail.com>
2023-08-25 11:39:41 -0300

    added scope is

--- criacao_tabelas_or.sql
+++ criacao_tabelas_or.sql
@@ -1,135 +1,235 @@
-CREATE OR REPLACE TYPE tp_regiao AS OBJECT(
-	cep VARCHAR2(9),
-  	estado VARCHAR2(20),
-  	cidade VARCHAR2(50),
-  	bairro VARCHAR2(50),
-  	rua VARCHAR2(50),
-    MEMBER FUNCTION cidade_natal RETURN VARCHAR2
+CREATE OR REPLACE TYPE tp_endereco AS OBJECT(
+    cep VARCHAR2(8),
+    estado VARCHAR2(50),
+    complemento VARCHAR2(50),
+    rua VARCHAR2(50),
+    numero NUMBER(5),
+    bairro VARCHAR2(50),
+    cidade VARCHAR2(50)
 );
 /
 
 CREATE OR REPLACE TYPE tp_fone AS OBJECT(
-    numero VARCHAR2(30)
+    numero VARCHAR2(11)
 );
+/
+
+CREATE OR REPLACE TYPE tp_nt_fone AS TABLE OF tp_fone;
 /
 
 CREATE OR REPLACE TYPE tp_fones AS VARRAY(5) OF tp_fone;
 /
 
 CREATE OR REPLACE TYPE tp_usuario AS OBJECT(
-    nome VARCHAR2(30),
-    email VARCHAR2(30),
-    regiao REF tp_regiao,
-    complemento VARCHAR2(50),
-    numero NUMBER,
-    fones tp_fones 
+    nome VARCHAR2(50),
+    email VARCHAR2(50),
+    endereco tp_endereco,
+    fones tp_fones,
+
+    CONSTRUCTOR FUNCTION tp_usuario(nome VARCHAR2, email VARCHAR2, endereco tp_endereco, fones tp_fones) RETURN SELF AS RESULT,
+
+    MEMBER PROCEDURE exibir_detalhes (SELF tp_usuario),
+    FINAL MEMBER PROCEDURE exibir_endereco_completo (SELF tp_usuario)
 ) NOT FINAL NOT INSTANTIABLE;
 /
 
+CREATE OR REPLACE TYPE BODY tp_usuario AS
+    CONSTRUCTOR FUNCTION tp_usuario(nome VARCHAR2, email VARCHAR2, endereco tp_endereco, fones tp_fones) RETURN SELF AS RESULT IS
+    BEGIN
+        SELF.nome := nome;
+        SELF.email := email;
+        SELF.endereco := endereco;
+        SELF.fones := fones;
+        RETURN;
+    END;
+
+    MEMBER PROCEDURE exibir_detalhes (SELF tp_usuario) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || SELF.nome);
+        DBMS_OUTPUT.PUT_LINE('Email: ' || SELF.email);
+        DBMS_OUTPUT.PUT_LINE('Rua: ' || SELF.endereco.rua);
+        DBMS_OUTPUT.PUT_LINE('Bairro: ' || SELF.endereco.bairro);
+        DBMS_OUTPUT.PUT_LINE('Cidade: ' || SELF.endereco.cidade);
+        FOR i IN 1..SELF.fones.COUNT LOOP
+            DBMS_OUTPUT.PUT_LINE('Fone ' || i || ': ' || SELF.fones(i).numero);
+        END LOOP;
+    END;
+
+    FINAL MEMBER PROCEDURE exibir_endereco_completo (SELF tp_usuario) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Rua: ' || SELF.endereco.rua);
+        DBMS_OUTPUT.PUT_LINE('Bairro: ' || SELF.endereco.bairro);
+        DBMS_OUTPUT.PUT_LINE('Cidade: ' || SELF.endereco.cidade);
+        DBMS_OUTPUT.PUT_LINE('Estado: ' || SELF.endereco.estado);
+        DBMS_OUTPUT.PUT_LINE('CEP: ' || SELF.endereco.cep);
+        DBMS_OUTPUT.PUT_LINE('Complemento: ' || SELF.endereco.complemento);
+        DBMS_OUTPUT.PUT_LINE('Numero: ' || SELF.endereco.numero);
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE tp_vendedor UNDER tp_usuario(
+    selo VARCHAR2(20),
+
+    MEMBER FUNCTION get_rank RETURN NUMBER,
+    OVERRIDING MEMBER PROCEDURE exibir_detalhes (SELF tp_vendedor)
+);
+/
+
+CREATE OR REPLACE TYPE BODY tp_vendedor AS
+    MEMBER FUNCTION get_rank RETURN NUMBER IS
+    BEGIN
+        IF SELF.selo = 'ouro' THEN
+            RETURN 1;
+        ELSIF SELF.selo = 'prata' THEN
+            RETURN 2;
+        ELSIF SELF.selo = 'bronze' THEN
+            RETURN 3;
+        ELSE
+            RETURN 4;
+        END IF;
+    END;
+
+    OVERRIDING MEMBER PROCEDURE exibir_detalhes (SELF tp_vendedor) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || SELF.nome);
+        DBMS_OUTPUT.PUT_LINE('Email: ' || SELF.email);
+        DBMS_OUTPUT.PUT_LINE('Rua: ' || SELF.endereco.rua);
+        DBMS_OUTPUT.PUT_LINE('Bairro: ' || SELF.endereco.bairro);
+        DBMS_OUTPUT.PUT_LINE('Cidade: ' || SELF.endereco.cidade);
+        FOR i IN 1..SELF.fones.COUNT LOOP
+            DBMS_OUTPUT.PUT_LINE('Fone ' || i || ': ' || SELF.fones(i).numero);
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('Selo: ' || SELF.selo);
+    END;
+END;
+/
+
 CREATE OR REPLACE TYPE tp_cartao_de_credito AS OBJECT(
-	cvv VARCHAR2(100),
-    numero NUMBER,
-    validade VARCHAR2(100)
+    cvv VARCHAR2(3),
+    numero VARCHAR2(16),
+    validade VARCHAR2(5)
 );
 /
 
 CREATE OR REPLACE TYPE tp_comprador UNDER tp_usuario(
-    cartao tp_cartao_de_credito
+    cartao tp_cartao_de_credito,
+    OVERRIDING MEMBER PROCEDURE exibir_detalhes (SELF tp_comprador)
 );
 /
 
-CREATE OR REPLACE TYPE tp_vendedor UNDER tp_usuario(
-    selo VARCHAR2(40)
+CREATE OR REPLACE TYPE BODY tp_comprador AS
+    OVERRIDING MEMBER PROCEDURE exibir_detalhes (SELF tp_comprador) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || SELF.nome);
+        DBMS_OUTPUT.PUT_LINE('Email: ' || SELF.email);
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE tp_favorita AS OBJECT(
+    vendedor REF tp_vendedor,
+    comprador REF tp_comprador
 );
 /
 
 CREATE OR REPLACE TYPE tp_motoboy AS OBJECT(
-	cpf VARCHAR2(50),
-    cnh VARCHAR2(50),
+    cpf VARCHAR2(11),
+    cnh VARCHAR2(11),
     cidade VARCHAR2(50),
-    cpf_indicador REF tp_motoboy,
-    selo VARCHAR2(10)
+    selo VARCHAR2(20),
+
+    MAP MEMBER FUNCTION qualidade RETURN NUMBER
 );
+/
+
+CREATE OR REPLACE TYPE BODY tp_motoboy AS
+    MAP MEMBER FUNCTION qualidade RETURN NUMBER IS
+    BEGIN
+        IF SELF.selo = 'ouro' THEN
+            RETURN 1;
+        ELSIF SELF.selo = 'prata' THEN
+            RETURN 2;
+        ELSIF SELF.selo = 'bronze' THEN
+            RETURN 3;
+        ELSE
+            RETURN 4;
+        END IF;
+    END;
+END;
+/
+
+ALTER TYPE tp_motoboy
+ADD ATTRIBUTE (indicador REF tp_motoboy) CASCADE;
 /
 
 CREATE OR REPLACE TYPE tp_cupom AS OBJECT(
-	cod NUMBER,
-	desconto NUMBER
+    cod NUMBER(5),
+    desconto NUMBER(3)
 );
 /
 
-CREATE OR REPLACE TYPE tp_favorita AS OBJECT (
-    email_vendedor REF tp_vendedor,
-    email_comprador REF tp_comprador
-);
-/
-
-CREATE OR REPLACE TYPE tp_anuncio AS OBJECT (
-    email_vendedor REF tp_vendedor,
-    cod NUMBER
-);
-/
-
-CREATE OR REPLACE TYPE tp_item AS OBJECT(
-    cod_anuncio REF tp_anuncio,
-    email_vendedor REF tp_vendedor,
-    tipo_produto VARCHAR2(100),
-    nome_do_produto VARCHAR2(100),
+-- aqui está do lado N da relação N:1
+CREATE OR REPLACE TYPE tp_anuncio AS OBJECT(
+    vendedor REF tp_vendedor,
+    codigo NUMBER(5),
+    tipo VARCHAR2(50),
+    nome VARCHAR2(50),
     descricao VARCHAR2(100),
-    preco NUMBER
+    preco NUMBER(5),
+
+    ORDER MEMBER FUNCTION e_maior (X tp_anuncio) RETURN NUMBER
 );
 /
 
-CREATE OR REPLACE TYPE tp_avalia AS OBJECT(
-    email_vendedor REF tp_vendedor,
-    email_comprador REF tp_comprador,
-    cod_anuncio REF tp_anuncio,
-    cpf_motoboy REF tp_motoboy,
-    nota_anuncio NUMBER,
-    nota_motoboy NUMBER
+CREATE OR REPLACE TYPE BODY tp_anuncio AS
+    ORDER MEMBER FUNCTION e_maior (X tp_anuncio) RETURN NUMBER IS
+    BEGIN
+        IF SELF.preco > X.preco THEN
+            RETURN 1;
+        ELSIF SELF.preco < X.preco THEN
+            RETURN -1;
+        ELSE
+            RETURN 0;
+        END IF;
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE tp_avaliar AS OBJECT(
+    anuncio REF tp_anuncio,
+    motoboy REF tp_motoboy,
+    comprador REF tp_comprador,
+    nota_anuncio NUMBER(1),
+    nota_motoboy NUMBER(1)
 );
 /
 
+-- mesma coisa do tipo a cima e o ngc do n:1 para no caso do motoboy e do cupom
 CREATE OR REPLACE TYPE tp_compra AS OBJECT(
-    email_vendedor REF tp_vendedor,
-    email_comprador REF tp_comprador,
-    cod_anuncio REF tp_anuncio,
-    cpf_motoboy REF tp_motoboy,
-    codigo_cupom REF tp_Cupom,
-    data_compra DATE,
-    data_entrega DATE
+    anuncio REF tp_anuncio,
+    comprador REF tp_comprador,
+    motoboy REF tp_motoboy,
+    cupom REF tp_cupom,
+    dt_compra DATE,
+    dt_entrega DATE
 );
 /
-
 
 -- TABELAS
-
-
-CREATE TABLE tb_regiao OF tp_regiao(
-    cep PRIMARY KEY
+CREATE TABLE tb_vendedor OF tp_vendedor(
+    email PRIMARY KEY
 );
 /
-
-CREATE TABLE tb_usuario OF tp_usuario(
-    email PRIMARY KEY,
-    regiao WITH ROWID REFERENCES tb_regiao
-);
-/
-
 
 CREATE TABLE tb_comprador OF tp_comprador(
-    email PRIMARY KEY REFERENCES tb_usuario
-);
-/
-
-CREATE TABLE tb_vendedor OF tp_vendedor(
-    email PRIMARY KEY REFERENCES tb_usuario
+    email PRIMARY KEY
 );
 /
 
 CREATE TABLE tb_motoboy OF tp_motoboy(
     cpf PRIMARY KEY,
-    cpf_indicador WITH ROWID REFERENCES tb_motoboy
+    indicador WITH ROWID REFERENCES tb_motoboy
 );
 /
 
@@ -137,46 +237,27 @@ CREATE TABLE tb_cupom OF tp_cupom(
     cod PRIMARY KEY
 );
 /
-
 CREATE TABLE tb_favorita OF tp_favorita(
-    email_vendedor WITH ROWID REFERENCES tb_vendedor,
-    email_comprador WITH ROWID REFERENCES tb_comprador,
-    PRIMARY KEY(email_vendedor, email_comprador)
+    vendedor WITH ROWID REFERENCES tb_vendedor,
+    comprador WITH ROWID REFERENCES tb_comprador
 );
 /
-
-
 CREATE TABLE tb_anuncio OF tp_anuncio(
-    email_vendedor WITH ROWID REFERENCES tb_vendedor,
-    cod PRIMARY KEY
+    vendedor SCOPE IS tb_vendedor
 );
 /
-
-CREATE TABLE tb_item OF tp_item(
-    cod_anuncio WITH ROWID REFERENCES tb_anuncio,
-    email_vendedor WITH ROWID REFERENCES tb_vendedor,
-    PRIMARY KEY(cod_anuncio, email_vendedor)
-);
-/
-
-CREATE TABLE tb_avalia OF tp_avalia(
-    email_vendedor WITH ROWID REFERENCES tb_vendedor,
-    email_comprador WITH ROWID REFERENCES tb_comprador,
-    cod_anuncio WITH ROWID REFERENCES tb_anuncio,
-    cpf_motoboy WITH ROWID REFERENCES tb_motoboy,
-    PRIMARY KEY(email_vendedor, email_comprador, cod_anuncio, cpf_motoboy)
+CREATE TABLE tb_avaliar OF tp_avaliar(
+    anuncio WITH ROWID REFERENCES tb_anuncio,
+    motoboy WITH ROWID REFERENCES tb_motoboy,
+    comprador WITH ROWID REFERENCES tb_comprador  
 );
 /
 
 CREATE TABLE tb_compra OF tp_compra(
-    email_vendedor WITH ROWID REFERENCES tb_vendedor,
-    email_comprador WITH ROWID REFERENCES tb_comprador,
-    cod_anuncio WITH ROWID REFERENCES tb_anuncio,
-    cpf_motoboy WITH ROWID REFERENCES tb_motoboy,
-    codigo_cupom WITH ROWID REFERENCES tb_cupom,
-    PRIMARY KEY(email_vendedor, email_comprador, cod_anuncio, cpf_motoboy)
+    anuncio WITH ROWID REFERENCES tb_anuncio,
+    motoboy WITH ROWID REFERENCES tb_motoboy,
+    comprador WITH ROWID REFERENCES tb_comprador,
+    cupom WITH ROWID REFERENCES tb_cupom    
 );
 /
 
-
-CREATE 
